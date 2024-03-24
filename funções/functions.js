908b7599fb861f98cbbeaba48f5176d8f1708d4d
FelipeDev30 <felipedevjob29@hotmail.com>
2024-03-24 12:41:58 -0300

    Declarando funções

--- funções/functions.js
+++ funções/functions.js
@@ -1,1 +1,25 @@
-console.log("Olá, mundo!");
+function getData(){
+    console.log("Pegando os dados do usuário!");
+}
+
+function cadastrandoUsuario(){
+    console.log("Usuário cadastrado com sucesso!");
+}
+
+function bemVindo(){
+    console.log("Seja Bem Vindo!");
+}
+
+function main(){
+    getData();
+    cadastrandoUsuario();
+    bemVindo();
+}
+
+main();
+
+/*
+
+Após declarar as funções necessárias para cadastrar usuário, foi criada uma função master para chamar todas as funções necessárias
+
+*/
