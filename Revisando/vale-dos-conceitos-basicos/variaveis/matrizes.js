9ebbe2d1bfe63a91df95bc6febcbfcda27b088d4
= <=>
2025-11-08 11:01:35 -0300

    Conceito de matrizes

---
+++ Revisando/vale-dos-conceitos-basicos/variaveis/matrizes.js
@@ -0,0 +1,31 @@
+/* 
+
+Matrizes (Arrays Multidimensionais)
+Uma matriz é uma coleção de arrays, onde cada array pode conter múltiplos valores. 
+Elas são úteis para armazenar dados estruturados, como tabelas ou listas de informações relacionadas.
+
+*/
+
+let pokemons = [
+    ['Pikachu', 'M', 'Level 25'],
+    ['Squirtle', 'F', 'Level 30'],
+    ['Psyduck', 'M', 'Level 20']
+]
+
+console.log("Matriz de Pokémons:");
+console.log(pokemons[0][0], "-", pokemons[0][1], "-", pokemons[0][2]);
+console.log(pokemons[1][0], "-", pokemons[1][1], "-", pokemons[1][2]);
+console.log(pokemons[2][0], "-", pokemons[2][1], "-", pokemons[2][2]);
+
+console.log('------------------------------------------------');
+
+let carros = [
+    ['Fusca', 'Volkswagen', '1970'],
+    ['Civic', 'Honda', '2020'],
+    ['Mustang', 'Ford', '2018']
+]
+
+console.log("Matriz de Carros:");
+console.log(carros[0][0], "-", carros[0][1], "-", carros[0][2]);
+console.log(carros[1][0], "-", carros[1][1], "-", carros[1][2]);
+console.log(carros[2][0], "-", carros[2][1], "-", carros[2][2]);
